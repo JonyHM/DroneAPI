938ab3d5ba9e02d95c5bb2b5dd2fd6ab0b3c9412
JonyHM <jonathas.moraes@fatec.sp.gov.br>
2019-06-09 18:41:37 -0300

    Versão estável finalizada

--- src/migration/1560048198496-CreateDrop.ts
+++ src/migration/1560048198496-CreateDrop.ts
@@ -3,10 +3,14 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class CreateDrop1560048198496 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query("CREATE TABLE IF NOT EXISTS drone (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, customer_image CHAR(255), customer_name CHAR(255), customer_adress CHAR(255), battery INTEGER NOT NULL, max_speed REAL NOT NULL, average_speed REAL NOT NULL, status CHAR(50), current_fly INTEGER NOT NULL, createdAt DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL);");
+        await queryRunner.query("CREATE TABLE IF NOT EXISTS drone (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, customer_image CHAR(255), customer_name CHAR(255), customer_adress CHAR(255), battery INTEGER NOT NULL, max_speed REAL NOT NULL, average_speed REAL NOT NULL, status CHAR(50), current_fly INTEGER NOT NULL, createdAt DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL);")
+            .then(() => console.log('Tabela de Drones criada!'))
+            .catch(err => console.log(err.message));
     }
     public async down(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query("DROP TABLE EXISTS drone;");
+        await queryRunner.dropTable("drone", true)
+            .then(() => console.log('Tabela de Drones apagada!'))
+            .catch(err => console.log(err.message));
     }
 
 }
